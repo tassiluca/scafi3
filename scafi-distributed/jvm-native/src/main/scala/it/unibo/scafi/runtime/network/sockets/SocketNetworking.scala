f1c25944564a1231636483c53a32a02750068bf4
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-06-26 23:27:03 +0200

    feat: add close and isOpen socket networking

--- scafi-distributed/jvm-native/src/main/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworking.scala
+++ scafi-distributed/jvm-native/src/main/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworking.scala
@@ -1,10 +1,27 @@
 package it.unibo.scafi.runtime.network.sockets
 
-import it.unibo.scafi.runtime.network.Serializable
+import java.net.{ ServerSocket, Socket }
+
+import scala.collection.Iterator.continually
 import scala.concurrent.Future
+import scala.util.Try
+import scala.concurrent.ExecutionContext
 
-trait SocketNetworking[T: Serializable] extends Networking[T, T] with InetTypes:
+import it.unibo.scafi.runtime.network.Serializable
 
-  override def in(port: Int)(onReceive: T => Unit): Future[ConnectionListener] = ???
+trait SocketNetworking[T: Serializable](using ExecutionContext) extends Networking[T, T] with InetTypes:
+
+  override def in(port: Int)(onReceive: T => Unit): Future[ConnectionListener] = Future.fromTry:
+    for
+      socketServer <- Try(ServerSocket(port))
+      connListener = new ConnectionListener:
+        val acceptLoop = Future:
+          continually(Try(socketServer.accept)).filter(_.isSuccess).foreach(s => serve(s.get))
+
+        private def serve(client: Socket) = ???
+
+        override def close(): Unit = socketServer.close()
+        override def isOpen: Boolean = !socketServer.isClosed
+    yield connListener
 
   override def out(endpoint: (String, Int)): Future[Connection] = ???
