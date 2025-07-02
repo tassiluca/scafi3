e8fba0706b3c1c1381d694bebd90eff4b43f2ede
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-02 11:36:55 +0200

    refactor: deferred execution network builders and keep track of accept future

--- scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworkingBehavior.scala
+++ scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworkingBehavior.scala
@@ -11,26 +11,31 @@ trait SocketNetworkingBehavior:
 
   given ExecutionContext = compiletime.deferred
 
-  def anInboundConnectionListener(networking: => Networking[String, String] & InetTypes) =
+  def anInboundConnectionListener(networking: => Networking[String, String]) =
     it should "be initializable on a specific port" in:
       val server = networking.in(FreePort)(nop)
-      whenReady(server): conn =>
-        conn.isOpen shouldBe true
-        conn.close()
-        conn.isOpen shouldBe false
+      whenReady(server()): connRef =>
+        val listener = connRef.listener
+        listener.isOpen shouldBe true
+        listener.close()
+        listener.isOpen shouldBe false
+        eventually(connRef.accept.isCompleted shouldBe true)
 
-  def anOutboundConnection(networking: => Networking[String, String] & InetTypes) =
+  def anOutboundConnection(networking: => Networking[String, String]) =
     it should "be able to connect to an available remote endpoint" in:
-      whenReady(networking.in(FreePort)(nop)): server =>
-        val client = networking.out(endpoint = (localhost, server.boundPort))
-        whenReady(client): conn =>
+      val server = networking.in(FreePort)(nop)
+      whenReady(server()): connRef =>
+        val listener = connRef.listener
+        val client = networking.out(endpoint = (localhost, listener.boundPort))
+        whenReady(client()): conn =>
           conn.isOpen shouldBe true
           conn.close()
           conn.isOpen shouldBe false
 
     it should "fail to connect to an unavailable remote endpoint" in:
       val port = 5050
-      whenReady(networking.out(endpoint = (localhost, port)).failed): ex =>
+      val client = networking.out(endpoint = (localhost, port))
+      whenReady(client().failed): ex =>
         ex shouldBe a[Throwable]
         ex.getMessage should (
           include("Connection refused") // Native
@@ -38,19 +43,19 @@ trait SocketNetworkingBehavior:
         )
   end anOutboundConnection
 
-  def both(networking: => Networking[String, String] & InetTypes) =
+  def both(networking: => Networking[String, String]) =
     it should "be able to accept incoming connections from remote endpoints" in:
       given PatienceConfig = PatienceConfig(timeout = Span(1, Seconds))
       val receivedMessages = scala.collection.mutable.ListBuffer.empty[String]
       val msg = "Hello, Scafi!"
       val exchange = for
-        server <- networking.in(FreePort)(receivedMessages.addOne)
-        client <- networking.out((localhost, server.boundPort))
+        server <- networking.in(FreePort)(receivedMessages.addOne)()
+        client <- networking.out((localhost, server.listener.boundPort))()
         _ <- client.send(msg)
       yield (server, client)
       whenReady(exchange): (server, client) =>
         eventually(receivedMessages should contain theSameElementsAs Seq(msg))
-        server.close()
+        server.listener.close()
         client.close()
 
   private val FreePort = 0
