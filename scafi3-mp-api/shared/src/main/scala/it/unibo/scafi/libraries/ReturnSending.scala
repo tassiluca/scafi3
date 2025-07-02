7237bb38c57b9c4900f9b6d83fd85f9381b637ab
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-02 12:22:35 +0200

    feat(mp-api): add support for using exchange and branching libraries from JS (#94)

---
+++ scafi3-mp-api/shared/src/main/scala/it/unibo/scafi/libraries/ReturnSending.scala
@@ -0,0 +1,54 @@
+package it.unibo.scafi.libraries
+
+import scala.scalajs.js.annotation.{ JSExport, JSExportTopLevel }
+
+/**
+ * Syntax sugar to return and send a value at the same time.
+ * @param returning
+ *   the value to return
+ * @param sending
+ *   the value to send
+ */
+@JSExportTopLevel("ReturnSending")
+case class ReturnSending[+Value](returning: Value, sending: Value)
+
+object ReturnSending:
+
+  /**
+   * An object of this class enables the syntax to send a different value from the one to return.
+   * @param returning
+   *   the value to return
+   */
+  case class Continuation[+Value](returning: Value):
+
+    /**
+     * The value to send.
+     * @param send
+     *   the value to send
+     * @return
+     *   a [[ReturnSending]] instance with the value to return and the value to send
+     */
+    @JSExport
+    infix def send[SendValue >: Value](send: SendValue): ReturnSending[SendValue] = ReturnSending(returning, send)
+
+  /**
+   * The value to return. It enables the syntax to send a different value from the one to return, using the `send`
+   * method of the [[Continuation]] class.
+   * @param value
+   *   the value to return
+   * @return
+   *   an instance of the [[Continuation]] class, on which the `send` method can be called
+   */
+  @JSExportTopLevel("returning")
+  inline def returning[Value](value: Value): Continuation[Value] = Continuation(value)
+
+  /**
+   * Alias for `ReturnSending(value, value)`.
+   * @param value
+   *   the value to return and send
+   * @return
+   *   a [[ReturnSending]] instance with the same value for returning and sending
+   */
+  @JSExportTopLevel("returnSending")
+  inline def returnSending[Value](value: Value): ReturnSending[Value] = ReturnSending(value, value)
+end ReturnSending
