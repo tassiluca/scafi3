a27bb21b5ce41332bcbe2795b54936548ddffbe5
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-02 11:36:55 +0200

    refactor: deferred execution network builders and keep track of accept future

--- scafi-distributed/jvm-native/src/main/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworking.scala
+++ scafi-distributed/jvm-native/src/main/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworking.scala
@@ -1,51 +1,57 @@
 package it.unibo.scafi.runtime.network.sockets
 
-import java.io.DataInputStream
-import java.net.{ ServerSocket, Socket }
-import java.io.DataOutputStream
+import java.net.{ ServerSocket, Socket, SocketException }
+import java.io.{ DataInputStream, DataOutputStream }
 
-import scala.collection.Iterator.continually
-import scala.concurrent.Future
-import scala.util.Try
-import scala.concurrent.ExecutionContext
-import scala.util.boundary
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.*
 import scala.util.boundary.break
 import scala.util.chaining.scalaUtilChainingOps
+import scala.annotation.tailrec
 
 import it.unibo.scafi.runtime.network.Serializable
-import it.unibo.scafi.runtime.network.Serializable.{ deserialize, serialize }
+import it.unibo.scafi.runtime.network.Serializable.*
 
 trait SocketNetworking[T: Serializable](using ExecutionContext) extends Networking[T, T] with InetTypes:
 
   private val MAX_MESSAGE_SIZE = 1024 * 1024
 
-  override def in(port: Port)(onReceive: T => Unit): Future[ConnectionListener] = Future.fromTry:
+  override def in(port: Port)(onReceive: T => Unit): () => Future[ListenerRef] = () =>
     for
-      socketServer <- Try(ServerSocket(port))
-      connListener = new ConnectionListener:
-        val acceptLoop = Future:
-          continually(Try(socketServer.accept)).filter(_.isSuccess).foreach(s => serve(s.get))
-        private def serve(client: Socket) =
-          val in = DataInputStream(client.getInputStream())
-          boundary:
-            while true do
-              val msgLen = in.readInt()
-              if msgLen < 0 || msgLen > MAX_MESSAGE_SIZE then
-                client.close()
-                break()
-              val buffer = new Array[Byte](msgLen)
-              in.readFully(buffer, 0, msgLen)
-              val msg = deserialize(buffer.take(msgLen))
-              onReceive(msg)
-          client.close()
-        override def close(): Unit = socketServer.close()
-        override def isOpen: Boolean = !socketServer.isClosed
-        override def boundPort: Port = socketServer.getLocalPort()
-    yield connListener
+      server <- Future.fromTry(Try(ServerSocket(port)))
+      serve = (client: Socket) =>
+        val in = DataInputStream(client.getInputStream())
+        boundary:
+          while true do
+            val msgLen = in.readInt()
+            if msgLen < 0 || msgLen > MAX_MESSAGE_SIZE then
+              client.close()
+              break()
+            val buffer = new Array[Byte](msgLen)
+            in.readFully(buffer, 0, msgLen)
+            val msg = deserialize(buffer.take(msgLen))
+            onReceive(msg)
+        client.close()
+      accept = Future:
+        @tailrec
+        def acceptLoop(): Unit =
+          Try(server.accept()) match
+            case Success(socket) =>
+              val _ = Future(serve(socket))
+              acceptLoop()
+            case Failure(_: SocketException) => ()
+            case _ => acceptLoop()
+        acceptLoop()
+      listener = new Listener:
+        override def close(): Unit = server.close()
+        override def isOpen: Boolean = !server.isClosed
+        override def boundPort: Port = server.getLocalPort()
+      listenerRef = ListenerRef(listener, accept)
+    yield listenerRef
 
-  override def out(endpoint: (Address, Port)): Future[Connection] = Future.fromTry:
+  override def out(endpoint: (Address, Port)): () => Future[Connection] = () =>
     for
-      socket <- Try(Socket(endpoint._1, endpoint._2))
+      socket <- Future.fromTry(Try(Socket(endpoint._1, endpoint._2)))
       conn = new Connection:
         override def send(msg: T): Future[Unit] = Future.fromTry:
           serialize(msg).pipe(s => Try(DataOutputStream(socket.getOutputStream()).tap(_.writeInt(s.length)).write(s)))
