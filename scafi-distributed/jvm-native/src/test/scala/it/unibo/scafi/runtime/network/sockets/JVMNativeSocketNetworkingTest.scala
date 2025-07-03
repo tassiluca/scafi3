35cd40972c087d54f440cf0d16e9664af92f8618
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-03 16:48:08 +0200

    refactor: cleaner logic

--- scafi-distributed/jvm-native/src/test/scala/it/unibo/scafi/runtime/network/sockets/JVMNativeSocketNetworkingTest.scala
+++ scafi-distributed/jvm-native/src/test/scala/it/unibo/scafi/runtime/network/sockets/JVMNativeSocketNetworkingTest.scala
@@ -6,13 +6,14 @@ import scala.concurrent.ExecutionContext
 
 class JVMNativeSocketNetworkingTest extends NetworkingTest with SocketNetworkingBehavior:
 
-  val networking = new SocketNetworking[String] {}
-
   override given ExecutionContext = ExecutionContext.fromExecutor(ForkJoinPool())
+
+  given SocketConfiguration = SocketConfiguration.basic
+
+  val networking = new SocketNetworking[String] {}
 
   it should behave like anInboundConnectionListener(networking)
 
   it should behave like anOutboundConnection(networking)
 
   it should behave like both(networking)
-end JVMNativeSocketNetworkingTest
