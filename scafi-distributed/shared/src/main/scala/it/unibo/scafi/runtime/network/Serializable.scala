8aa0b351826009de77486f516d8eec4a1b03c2b7
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-03 16:48:08 +0200

    refactor: cleaner logic

--- scafi-distributed/shared/src/main/scala/it/unibo/scafi/runtime/network/Serializable.scala
+++ scafi-distributed/shared/src/main/scala/it/unibo/scafi/runtime/network/Serializable.scala
@@ -1,15 +1,61 @@
 package it.unibo.scafi.runtime.network
 
-trait Serializable[T]:
+/**
+ * A type class for serializing and deserializing messages.
+ * @tparam Message
+ *   the type of messages to serialize and deserialize.
+ */
+trait Serializable[Message]:
 
-  def serialize(t: T): Array[Byte]
+  /**
+   * Serializes a message into an array of bytes.
+   * @param message
+   *   the message to serialize.
+   * @return
+   *   an array of bytes representing the serialized message.
+   */
+  def serialize(message: Message): Array[Byte]
 
-  def deserialize(bytes: Array[Byte]): T
+  /**
+   * Deserializes an array of bytes into a message.
+   * @param bytes
+   *   the array of bytes to deserialize.
+   * @return
+   *   the deserialized message.
+   */
+  def deserialize(bytes: Array[Byte]): Message
 
 object Serializable:
-  def serialize[T](t: T)(using s: Serializable[T]): Array[Byte] = s.serialize(t)
-  def deserialize[T](bytes: Array[Byte])(using s: Serializable[T]): T = s.deserialize(bytes)
 
+  /**
+   * Serializes the given message into an array of bytes using a contextually available `Serializable` instance.
+   * @param message
+   *   the message to serialize.
+   * @param s
+   *   the contextually available `Serializable` instance for the message type.
+   * @tparam Message
+   *   the type of the message to serialize.
+   * @return
+   *   an array of bytes representing the serialized message.
+   */
+  def serialize[Message](message: Message)(using s: Serializable[Message]): Array[Byte] = s.serialize(message)
+
+  /**
+   * Deserializes the given array of bytes into a message.
+   * @param bytes
+   *   the array of bytes to deserialize.
+   * @param s
+   *   the contextually available `Serializable` instance for the message type.
+   * @tparam Message
+   *   the type of the message to deserialize.
+   * @return
+   *   the deserialized message.
+   */
+  def deserialize[Message](bytes: Array[Byte])(using s: Serializable[Message]): Message = s.deserialize(bytes)
+
+  /** A default `Serializable` instance for `String` messages. */
   given Serializable[String] with
     def serialize(t: String): Array[Byte] = t.getBytes
     def deserialize(bytes: Array[Byte]): String = String(bytes)
+
+end Serializable
