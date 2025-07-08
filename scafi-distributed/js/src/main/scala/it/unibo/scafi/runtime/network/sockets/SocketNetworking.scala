2212629ca964fe1baaff32666814649a0dc553dd
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-08 16:08:14 +0200

    refactor: factor out common out logic

--- scafi-distributed/js/src/main/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworking.scala
+++ scafi-distributed/js/src/main/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworking.scala
@@ -1,78 +1,81 @@
 package it.unibo.scafi.runtime.network.sockets
 
-import java.util.concurrent.atomic.AtomicBoolean
+import java.nio.ByteBuffer
 
+import scala.LazyList.continually
+import scala.scalajs.js
+import scala.scalajs.js.typedarray.Uint8Array
 import scala.concurrent.{ ExecutionContext, Future, Promise }
-import scala.scalajs.js.typedarray.{ ArrayBuffer, DataView, Uint8Array }
-import scala.util.boundary
-import scala.util.boundary.break
+import scala.collection.mutable.ArrayBuffer
+import scala.util.{ Failure, Success, Try }
+import scala.util.chaining.scalaUtilChainingOps
 
 import it.unibo.scafi.runtime.network.Serializable
 import it.unibo.scafi.runtime.network.Serializable.*
+import it.unibo.scafi.runtime.network.sockets.EventEmitter.*
 
-@SuppressWarnings(Array("scalafix:DisableSyntax.asInstanceOf"))
 trait SocketNetworking[Message: Serializable](using ec: ExecutionContext, conf: SocketConfiguration)
-    extends Networking[Message, Message]:
+    extends NetworkingTemplate[Message]:
 
-  object Events:
-    val connect = "connect"
-    val error = "error"
-    val listening = "listening"
-    val data = "data"
+  override def out(endpoint: Endpoint): () => Future[Connection] = () =>
+    for
+      socket <- createSocket(endpoint)
+      conn = new ConnectionTemplate:
+        override def write(buffer: Array[Byte]): Future[Unit] = fromPromise: p =>
+          val data = new Uint8Array(buffer.length)
+          for i <- buffer.indices do data(i) = buffer(i)
+          socket.write(data):
+            case e: js.Error => p.tryFailure(Exception(e.message)): Unit
+            case _ => p.trySuccess(()): Unit
+        override def isOpen: Boolean = !socket.destroyed
+        override def close(): Unit = socket.destroy()
+    yield conn
 
-  import Events.*
-
-  override def out(endpoint: (String, Int)): () => Future[Connection] = () =>
-    def createConnection(socket: Socket): Connection = new Connection:
-      override def close(): Unit = socket.destroy()
-      override def isOpen: Boolean = !socket.destroyed
-      override def send(msg: Message): Future[Unit] =
-        if !isOpen then Future.failed(IllegalStateException("Connection is closed!"))
-        else
-          val promise = Promise[Unit]()
-          val serializedMsg = serialize(msg)
-          val buffer = new ArrayBuffer(4 + serializedMsg.length)
-          val view = new DataView(buffer)
-          view.setUint32(0, serializedMsg.length)
-          val data = new Uint8Array(buffer)
-          for i <- serializedMsg.indices do data(4 + i) = serializedMsg(i)
-          socket.write(data)(Option(_).fold(promise.trySuccess(()))(err => promise.tryFailure(Exception(err))))
-          promise.future
-    val promisedConnection = Promise[Connection]()
+  private def createSocket(endpoint: Endpoint): Future[Socket] = fromPromise: p =>
     val socket = Net.connect(endpoint._2, endpoint._1)
-    socket.on(connect)(_ => promisedConnection.trySuccess(createConnection(socket)): Unit)
-    socket.on(error): err =>
-      promisedConnection.tryFailure(Exception(err.asInstanceOf[scala.scalajs.js.Dynamic].message.toString)): Unit
-      socket.destroy()
-    promisedConnection.future
+    socket.onceConnect(() => p.trySuccess(socket): Unit)
+    socket.onError(err => p.tryFailure(Exception(err.message)).pipe(_ => socket.destroy()))
 
-  override def in(port: Int)(onReceive: Message => Unit): () => Future[ListenerRef] = () =>
-    val connListenerPromise = Promise[Listener]()
-    val acceptPromise = Promise[Unit]()
-    def react(data: Uint8Array): Unit = boundary:
-      var offset = 0
-      while data.byteLength - offset >= 4 do
-        val view = new DataView(data.buffer, data.byteOffset + offset, data.byteLength - offset)
-        val msgLen = view.getUint32(0).toInt
-        if msgLen <= 0 || msgLen > data.byteLength - offset - 4 || msgLen > conf.maxMessageSize then break(())
-        val messageBytes = new Uint8Array(data.buffer, data.byteOffset + offset + 4, msgLen)
-        val bytes = new Array[Byte](msgLen)
-        for i <- 0 until msgLen do bytes(i) = messageBytes(i).toByte
-        onReceive(deserialize(bytes))
-        offset += 4 + msgLen
-    val serverSocket = Net.createServer(_.on(data)(data => react(data.asInstanceOf[Uint8Array])))
-    val listener = new Listener:
-      private val open = AtomicBoolean(true)
-      override def boundPort: Port = serverSocket.address().port
-      override def close(): Unit =
-        open.set(false)
-        serverSocket.close()
-        acceptPromise.trySuccess(()): Unit
-      override def isOpen: Boolean = open.get()
-    serverSocket.on(listening)(_ => connListenerPromise.trySuccess(listener): Unit)
-    serverSocket.on(error): err =>
-      connListenerPromise.tryFailure(Exception(err.asInstanceOf[scala.scalajs.js.Dynamic].message.toString)): Unit
+  override def in(port: Port)(onReceive: Message => Unit): () => Future[ListenerRef] = () =>
+    val listener = ServerSocketListener(onReceive)
+    fromPromise[Listener]: p =>
+      listener.serverSocket.onError: err =>
+        p.tryFailure(Exception(err.message)): Unit
+        listener.serverSocket.close()
+      listener.serverSocket.listen(port)(() => p.trySuccess(listener): Unit)
+    .map(ListenerRef(_, listener.acceptPromise.future))
+
+  private class ServerSocketListener(onReceive: Message => Unit) extends Listener:
+    private var open = true
+    private val buffer = ArrayBuffer[Byte]()
+    val acceptPromise: Promise[Unit] = Promise[Unit]()
+    val serverSocket: Server = Net.createServer(socket => socket.onData(serve(_)(using socket)))
+
+    private def serve(chunk: Uint8Array)(using socket: Socket): Unit =
+      for i <- 0 until chunk.length do buffer += chunk(i).toByte
+      continually(validate(readMessageLength))
+        .takeWhile(msgLen => msgLen.isSuccess && buffer.length >= Integer.BYTES + msgLen.get)
+        .collect { case Success(value) => value }
+        .filter(_ > 0)
+        .foreach: msgLen =>
+          val msgBytes = buffer.slice(Integer.BYTES, Integer.BYTES + msgLen).toArray
+          buffer.remove(0, Integer.BYTES + msgLen)
+          onReceive(deserialize(msgBytes))
+
+    private def readMessageLength: Try[Int] = Try(ByteBuffer.wrap(buffer.slice(0, Integer.BYTES).toArray).getInt)
+
+    private def validate(msgLen: Try[Int])(using socket: Socket): Try[Int] = msgLen.flatMap: rawLen =>
+      Try(rawLen ensuring (_ < conf.maxMessageSize)).recoverWith(Failure(_).tap(_ => socket.destroy()))
+
+    override def boundPort: Port = serverSocket.address().port
+
+    override def close(): Unit =
+      open = false
       serverSocket.close()
-    serverSocket.listen(port)
-    connListenerPromise.future.map(ListenerRef(_, acceptPromise.future))
+      acceptPromise.trySuccess(()): Unit
+
+    override def isOpen: Boolean = open
+  end ServerSocketListener
+
+  private def fromPromise[T](logic: Promise[T] => Unit): Future[T] = Promise[T].tap(logic).future
 end SocketNetworking
