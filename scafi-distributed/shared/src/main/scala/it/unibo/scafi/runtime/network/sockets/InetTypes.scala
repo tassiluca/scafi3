7ea280e4b389d19055ecb84f5575d14350291e0a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-09 18:35:09 +0200

    refactor: use refinement for inet types

--- scafi-distributed/shared/src/main/scala/it/unibo/scafi/runtime/network/sockets/InetTypes.scala
+++ scafi-distributed/shared/src/main/scala/it/unibo/scafi/runtime/network/sockets/InetTypes.scala
@@ -1,13 +1,58 @@
 package it.unibo.scafi.runtime.network.sockets
 
-// TODO: program with opaque types and safe constructors.
-trait InetTypes:
-  type Address = String
+import io.github.iltotore.iron.*
+import io.github.iltotore.iron.constraint.numeric.Interval
+import io.github.iltotore.iron.constraint.string.Match
 
-  type Port = Int
-
-  type Endpoint = (Address, Port)
-
+/**
+ * Defines types for internet-based networking.
+ */
 object InetTypes:
+  export io.github.iltotore.iron.{ assume, autoRefine }
 
-  val localhost = "localhost"
+  // IP addresses validation is performed using https://www.ditig.com/validating-ipv4-and-ipv6-addresses-with-regexp
+
+  /** An IPv4 address. */
+  type IPv4 = DescribedAs[
+    Match["^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$"],
+    "Must be a valid IPv4 address consisting of four decimal numbers (0-255) separated by dots, e.g., 192.168.1.1.",
+  ]
+
+  /** An IPv6 address. */
+  type IPv6 = DescribedAs[
+    Match[
+      "^((?:[0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|(?:[0-9A-Fa-f]{1,4}:){1,7}:|:(?::[0-9A-Fa-f]{1,4}){1,7}|(?:[0-9A-Fa-f]{1,4}:){1,6}:[0-9A-Fa-f]{1,4}|(?:[0-9A-Fa-f]{1,4}:){1,5}(?::[0-9A-Fa-f]{1,4}){1,2}|(?:[0-9A-Fa-f]{1,4}:){1,4}(?::[0-9A-Fa-f]{1,4}){1,3}|(?:[0-9A-Fa-f]{1,4}:){1,3}(?::[0-9A-Fa-f]{1,4}){1,4}|(?:[0-9A-Fa-f]{1,4}:){1,2}(?::[0-9A-Fa-f]{1,4}){1,5}|[0-9A-Fa-f]{1,4}:(?:(?::[0-9A-Fa-f]{1,4}){1,6})|:(?:(?::[0-9A-Fa-f]{1,4}){1,6}))$",
+    ],
+    "Must be a valid IPv6 address with eight groups of hexadecimal digits, optionally compressed with ::, or IPv4-mapped IPv6.",
+  ]
+
+  /** A valid hostname. */
+  type Hostname = DescribedAs[
+    Match[
+      "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$",
+    ],
+    "Must be a valid hostname.",
+  ]
+
+  /** A valid network address, either an IPv4, IPv6 address, or a valid hostname. */
+  type Address = String :|
+    DescribedAs[IPv4 | IPv6 | Hostname, "A valid IP address, either IPv4 or IPv6, or a hostname."]
+
+  /** A network port number. Valid values are in the range [0, 65535). */
+  type Port = Int :| Interval.ClosedOpen[0, 65_536]
+
+  /**
+   * A generic network endpoint.
+   * @param address
+   *   a valid network [[Address]].
+   * @param port
+   *   a valid [[Port]] number.
+   */
+  case class Endpoint(address: Address, port: Port)
+
+  /** Localhost address. */
+  val Localhost: Address = "localhost"
+
+  /** The port used to instruct the operating system to assign a free ephemeral port. */
+  val FreePort: Port = 0
+end InetTypes
