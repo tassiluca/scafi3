66405bb70df7a8841f9249a8b4472e5e3430b06d
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-16 17:22:17 +0200

    test: improve tests with utility functions

--- scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworkingBehavior.scala
+++ scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/sockets/SocketNetworkingBehavior.scala
@@ -9,23 +9,21 @@ import it.unibo.scafi.runtime.network.sockets.InetTypes.*
 
 trait SocketNetworkingBehavior extends NetworkingTest:
 
-  def anInboundConnectionListener(networking: => Networking[String, String])(using ExecutionContext): Unit =
+  type Message = String
+
+  def anInboundConnectionListener(using networking: Networking[Message, Message])(using ExecutionContext): Unit =
     it should "be able to be initialized on a free port" in:
       val server = networking.in(FreePort)(nop)
-      server.run() map: ref =>
+      server.run() verify: ref =>
         ref.listener.isOpen shouldBe true
         ref.listener.close()
         ref.listener.isOpen shouldBe false
 
-  def anOutboundConnection(networking: => Networking[String, String]): Unit =
+  def anOutboundConnection(using networking: Networking[Message, Message]): Unit =
     it should "be able to connect to an available remote endpoint" in:
-      for
-        server <- networking.in(FreePort)(nop).run()
-        client <- networking.out(Endpoint(Localhost, server.listener.boundPort)).run()
-      yield
+      usingServer(nop): client =>
         client.isOpen shouldBe true
         client.close()
-        server.listener.close()
         client.isOpen shouldBe false
 
     it should "fail to connect to an unavailable remote endpoint" in:
@@ -34,31 +32,38 @@ trait SocketNetworkingBehavior extends NetworkingTest:
       client.run().failed map:
         _ shouldBe a[Throwable]
 
-  def both(networking: => Networking[String, String]) =
+  def both(using networking: Networking[Message, Message]) =
     it should "be able to accept incoming connections from remote endpoints" in:
       val receivedMessages = CopyOnWriteArrayList[String]()
       val messages = Seq("Hello", "World", "Scafi", "Networking")
-      for
-        server <- networking.in(FreePort)(msg => receivedMessages.add(msg): Unit).run()
-        client <- networking.out(Endpoint(Localhost, server.listener.boundPort)).run()
-        _ <- messages.foldLeft(Future.unit)((acc, msg) => acc.flatMap(_ => client.send(msg)))
-        _ = client.close()
-        assertion <- eventually(timeout = 2.seconds, interval = 250.millis):
-          receivedMessages should contain theSameElementsInOrderAs messages
-        _ = server.listener.close()
-      yield assertion
+      usingServer(msg => receivedMessages.add(msg): Unit):
+        _ send messages verifying:
+          eventually(timeout = 2.seconds, interval = 250.millis):
+            receivedMessages should contain theSameElementsInOrderAs messages
 
     it should "close connections with clients attempting to flood the server" in:
       val tooLargeMessage = "A" * 65_536
-      for
-        server <- networking.in(FreePort)(nop).run()
-        client <- networking.out(Endpoint(Localhost, server.listener.boundPort)).run()
-        _ <- client.send(tooLargeMessage)
-        assertion <- eventually(timeout = 500.millis, interval = 100.millis):
-          client.isOpen shouldBe false
-        _ = server.listener.close()
-      yield assertion
+      usingServer(nop): client =>
+        client send Seq(tooLargeMessage) verifying:
+          eventually(timeout = 500.millis, interval = 100.millis):
+            client.isOpen shouldBe false
   end both
 
+  private def usingServer[Result](using
+      net: Networking[Message, Message],
+  )(onMessage: Message => Unit)(todo: net.Connection => Future[Result]) =
+    for
+      server <- net.in(FreePort)(onMessage).run()
+      client <- net.out(Endpoint(Localhost, server.listener.boundPort)).run()
+      result <- todo(client)
+      _ = client.close()
+      _ = server.listener.close()
+    yield result
+
+  extension (using net: Networking[Message, Message])(client: net.Connection)
+    infix def send(messages: Seq[Message]) =
+      messages.foldLeft(Future.unit)((acc, msg) => acc.flatMap(_ => client.send(msg)))
+
   private val nop: Any => Unit = _ => ()
+
 end SocketNetworkingBehavior
