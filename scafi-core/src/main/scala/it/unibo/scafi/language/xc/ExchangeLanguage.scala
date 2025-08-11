fe37413d1765f5e06cf80915692b1f16da1e19c1
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-08-11 23:21:57 +0200

    feat!: use in memory codable for evolve

--- scafi-core/src/main/scala/it/unibo/scafi/language/xc/ExchangeLanguage.scala
+++ scafi-core/src/main/scala/it/unibo/scafi/language/xc/ExchangeLanguage.scala
@@ -4,7 +4,7 @@ import it.unibo.scafi.language.fc.syntax.FieldCalculusSyntax
 import it.unibo.scafi.language.xc.calculus.ExchangeCalculus
 import it.unibo.scafi.language.xc.syntax.ReturnSending.returning
 import it.unibo.scafi.language.xc.syntax.{ ExchangeSyntax, ReturnSending }
-import it.unibo.scafi.message.CodableFromTo
+import it.unibo.scafi.message.{ CodableFromTo, Codables }
 
 /**
  * Exchange language provides the syntax for the exchange calculus main construct: `exchange`, but also provides the
@@ -23,12 +23,15 @@ trait ExchangeLanguage extends ExchangeSyntax, FieldCalculusSyntax:
   override def neighborValues[Format, Value: CodableFromTo[Format]](expr: Value): SharedData[Value] =
     exchange(expr)(nv => returning(nv) send expr)
 
-  override def evolve[Format, Value: CodableFromTo[Format]](initial: Value)(evolution: Value => Value): Value =
+  override def evolve[Value](initial: Value)(evolution: Value => Value): Value =
+    // here exchange is used to evolve the value: `None` is shared with neighbors, therefore,
+    // codable for in memory communications can be used
     exchange(None)(nones =>
       val previousValue = nones(localId).getOrElse(initial)
       nones.set(localId, Some(evolution(previousValue))),
-    )(localId).get
+    )(using Codables.forInMemoryCommunications)(localId).get
 
   override def share[Format, Value: CodableFromTo[Format]](initial: Value)(
       shareAndReturning: SharedData[Value] => Value,
   ): Value = exchange(initial)(nv => shareAndReturning(nv))(localId)
+end ExchangeLanguage
