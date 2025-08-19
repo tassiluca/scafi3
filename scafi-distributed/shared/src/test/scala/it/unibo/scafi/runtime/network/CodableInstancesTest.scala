aba5577a4649f166afbf12f730a7c6b2bfc1b9a6
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-08-19 23:39:00 +0200

    test: codables for valuetrees

--- scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/CodableInstancesTest.scala
+++ scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/CodableInstancesTest.scala
@@ -1,12 +1,14 @@
 package it.unibo.scafi.runtime.network
 
-import it.unibo.scafi.message.Path
+import it.unibo.scafi.message.{ Path, ValueTree }
 import it.unibo.scafi.runtime.network.CodableInstances.given
 import it.unibo.scafi.utils.InvocationCoordinate
 import it.unibo.scafi.message.Codable.*
 
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
+
+import unsafeExceptions.canThrowAny
 
 class CodableInstancesTest extends AnyWordSpec with should.Matchers:
 
@@ -19,6 +21,28 @@ class CodableInstancesTest extends AnyWordSpec with should.Matchers:
     "composed of other tokens" should:
       "fail to encode and decode" in:
         case class Unsupported(value: String)
-        val path = Path(Unsupported("branch/true"), Unsupported("exchange"))
-        an[Throwable] should be thrownBy encode(path)
-        an[Throwable] should be thrownBy decode[Array[Byte], Path](encode(path))
+        val unsupportedToken = Unsupported("branch/true")
+        (the[Exception] thrownBy encode(Path(unsupportedToken))).getMessage should include(
+          s"Unsupported Path token type ${unsupportedToken.getClass.getName}",
+        )
+        an[Exception] should be thrownBy decode[Array[Byte], Path]("Unsupported path".getBytes)
+
+  "ValueTree" when:
+    "composed of already encoded values" should:
+      "be both encodable and decodable" in:
+        val path = Path(InvocationCoordinate("branch/true", 0), InvocationCoordinate("exchange", 0))
+        val value = "value"
+        val decoded = decode[Array[Byte], ValueTree](encode(ValueTree(Map(path -> value.getBytes))))
+        new String(decoded[Array[Byte]](path)) shouldBe value
+
+    "when composed of non-encoded values" should:
+      "discard them during encoding" in:
+        val trueBranch = Path(InvocationCoordinate("branch/true", 0), InvocationCoordinate("exchange", 0))
+        val falseBranch = Path(InvocationCoordinate("branch/false", 0), InvocationCoordinate("exchange", 0))
+        val trueBranchValue = "value1"
+        val falseBranchValue = None
+        val entries = Map(trueBranch -> trueBranchValue.getBytes, falseBranch -> falseBranchValue)
+        val decoded = decode[Array[Byte], ValueTree](encode(ValueTree(entries)))
+        new String(decoded[Array[Byte]](trueBranch)) shouldBe trueBranchValue
+        decoded.get(falseBranch) shouldBe None
+end CodableInstancesTest
