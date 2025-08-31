2ca9c3bd60613f9041647dac473321e54626b8d3
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-08-31 11:45:15 +0200

    feat: first draft working version

---
+++ scafi-mp-api/js/src/main/scala/it/unibo/scafi/libraries/JSScafiRuntime.scala
@@ -0,0 +1,48 @@
+package it.unibo.scafi.libraries
+
+import scala.concurrent.ExecutionContext
+import scala.scalajs.js.annotation.JSExportTopLevel
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.Future
+
+import it.unibo.scafi.libraries.bindings.ScafiNetworkBinding
+import it.unibo.scafi.message.RegisterableCodable
+import it.unibo.scafi.presentation.JSBinaryCodable.jsBinaryCodable
+import it.unibo.scafi.runtime.ScafiEngine
+import it.unibo.scafi.context.xc.ExchangeAggregateContext.exchangeContextFactory
+import it.unibo.scafi.runtime.network.sockets.ConnectionOrientedNetworkManager
+import it.unibo.scafi.utils.Platform
+
+object JSScafiRuntime extends PortableRuntime with ScafiNetworkBinding with JSTypes:
+
+  override given ExecutionContext = scalajs.concurrent.JSExecutionContext.Implicits.queue
+
+  override given [Value, Format]: RegisterableCodable[Value, Format] =
+    jsBinaryCodable.asInstanceOf[RegisterableCodable[Value, Format]]
+
+  @JSExportTopLevel("Runtime")
+  object JSInterface extends Interface with ADTs with NetworkBindings:
+
+    @JSExport
+    def engine[ID, Result](
+        deviceId: ID,
+        network: ConnectionOrientedNetworkManager[ID],
+        program: Function1[FullLibrary, Result],
+    ): Future[Unit] =
+      def loop(): Future[Unit] =
+        for
+          engine = ScafiEngine(deviceId, network, exchangeContextFactory)(program(FullLibrary()))
+          result <- Future(engine.cycle())
+          _ = scribe.info(s"Node $deviceId cycle result: $result")
+          _ <- Platform.asyncOps.sleep(1.second)
+          _ = scribe.info(s"Node $deviceId waking up")
+          _ <- loop()
+        yield ()
+      network
+        .start()
+        .andThen(res => scribe.info("Network started " + res))
+        .flatMap(_ => loop())
+        .andThen(res => scribe.info(res.toString))
+    end engine
+  end JSInterface
+end JSScafiRuntime
---
+++ scafi-mp-api/js/src/test/js/real1.mjs
@@ -0,0 +1,22 @@
+"use strict";
+
+import { Runtime, returnSending } from '../../../target/scala-3.6.4/scafi-mp-api-fastopt/main.mjs'
+
+console.log("::: SCAFI3 PROGRAM :::")
+
+const deviceId = 1
+const port = 5051
+const neighbors = new Map([
+    [2, Runtime.Endpoint("localhost", 5052)],
+])
+
+console.log("::: Info :::");
+console.log("Device ID: " + deviceId);
+console.log("Port:      " + port);
+
+function aggregateProgram(lang) {
+    return lang.exchange(lang.Field.of(lang.localId), n => returnSending(n));
+}
+
+const network = Runtime.socketNetwork(deviceId, port, neighbors);
+Runtime.engine(deviceId, network, lang => aggregateProgram(lang));
