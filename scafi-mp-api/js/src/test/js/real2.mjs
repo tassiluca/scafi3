76c9d07ab1050fad4623a6794bea8d2f3918d56c
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-09-01 18:07:57 +0200

    text: example with protobuf

--- scafi-mp-api/js/src/test/js/real2.mjs
+++ scafi-mp-api/js/src/test/js/real2.mjs
@@ -1,6 +1,7 @@
 "use strict";
 
 import { Runtime, returnSending } from '../../../target/scala-3.6.4/scafi-mp-api-fastopt/main.mjs'
+import proto from './foo.js';
 
 console.log("::: SCAFI3 PROGRAM :::")
 
@@ -14,12 +15,21 @@ console.log("::: Info :::");
 console.log("Device ID: " + deviceId);
 console.log("Port:      " + port);
 
+const foo = proto.Foo.create({ name: "Bob", id: 22 });
+
+function prettyPrint(foo) {
+    return `Foo(name=${foo.name}, id=${foo.id})`;
+}
+
 function aggregateProgram(lang) {
-    return lang.branch(
+    const all = lang.branch(
         deviceId % 2 == 0, 
         () => lang.exchange(lang.Field.of(true), n => returnSending(n)),
-        () => lang.exchange(lang.Field.of(false), n => returnSending(n)),
+        () => lang.exchange(lang.Field.of(foo), n => returnSending(n)),
     );
+    return `Default: ${prettyPrint(all.default)}, Neighbors: ${
+        Array.from(all.neighborValues, ([k, v]) => `${k}: ${prettyPrint(v)}`).join(", ")
+    }`;
 }
 
 const network = Runtime.socketNetwork(deviceId, port, neighbors);
