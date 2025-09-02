905f9fa75a544529c54013b97dbc6eee5a12f22c
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-09-02 18:38:58 +0200

    test: js test infrastructure

--- scafi-mp-api/js/src/test/scala/it/unibo/scafi/runtime/JSPlatformTest.scala
+++ scafi-mp-api/js/src/test/scala/it/unibo/scafi/runtime/JSPlatformTest.scala
@@ -1,22 +1,26 @@
 package it.unibo.scafi.runtime
 
-import scalajs.js
+import scala.util.{ Success, Try }
 
-import scala.util.Try
-import scala.util.Success
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import io.github.iltotore.iron.autoRefine
+
+import scalajs.js
 
 class JSPlatformTest extends AnyFlatSpec with PlatformTest with should.Matchers:
 
   val rootProjectPath = NodeProcess.cwd()
-  val testSnapshotsPath = s"$rootProjectPath/scafi-mp-api/js/src/test/resources/snapshots/js"
+  override val snapshotsFolderPath: Path = s"$rootProjectPath/scafi-mp-api/js/src/test/resources/snapshots/js"
   val testResourcesPath = s"$rootProjectPath/scafi-mp-api/js/src/test/resources/js"
-  val templateFilePath = s"$testResourcesPath/program.template.mjs"
+  override val programTemplatePath = s"$testResourcesPath/program.template.mjs"
   val packageFilePath = s"$testResourcesPath/package.json"
+  override val templatePaths: Set[Path] = Set(programTemplatePath, packageFilePath)
 
   it should "work" in:
-    test("js-first-test")
+    test("simple-exchange"):
+      "{{deviceId}}" -> "10"
+      "{{port}}" -> "100"
 
   override def testDir(directoryName: String): Path =
     val osTmpDir = OS.tmpdir()
@@ -26,21 +30,18 @@ class JSPlatformTest extends AnyFlatSpec with PlatformTest with should.Matchers:
     scribe.info(s"Temporary directory created at: $tempDir")
     tempDir
 
-  override def resolveTemplates(testName: String, workingDir: Path): Unit =
-    freshProjectCopy(workingDir)
-    val templateFile = s"$workingDir/program.template.mjs"
-    val content = FS.readFileSync(templateFile, "utf-8")
-    val updatedContent = content.replace("{{deviceId}}", "test-device-id")
-    FS.writeFileSync(s"$workingDir/main.mjs", updatedContent)
+  override def read(path: Path): String = FS.readFileSync(path, "utf-8")
 
-  private def freshProjectCopy(workingDir: Path): Unit =
-    val resources = List(templateFilePath, packageFilePath)
-    resources.foreach: file =>
-      val target = s"$workingDir/${Path.basename(file)}"
-      FS.copyFileSync(file, target)
+  override def write(path: Path, content: String): Unit = FS.writeFileSync(path, content)
+
+  override def copy(origin: Path, destination: Path): Unit = FS.copyFileSync(origin, destination)
+
+  override def baseName(path: Path): String = Path.basename(path)
+
+  override def programContentPath(testName: String): Path = s"$testResourcesPath/$testName/program.mjs"
 
   override def compile(workingDir: Path): Try[Unit] = Success(())
 
   override def run(workingDir: Path): Try[String] = Try:
-    ChildProcess.execSync("node main.mjs", js.Dictionary("cwd" -> workingDir)).toString()
+    ChildProcess.execSync("node main.mjs", js.Dictionary(("cwd", workingDir))).toString()
 end JSPlatformTest
--- scafi-mp-api/shared/src/test/scala/it/unibo/scafi/runtime/PlatformTest.scala
+++ scafi-mp-api/shared/src/test/scala/it/unibo/scafi/runtime/PlatformTest.scala
@@ -2,28 +2,81 @@ package it.unibo.scafi.runtime
 
 import scala.util.Try
 
-trait PlatformTest:
+import it.unibo.scafi.runtime.PlatformTest.{ Path, Pattern }
+import it.unibo.scafi.runtime.PlatformTest.SubstitutionBuilder
 
-  type Path = String
+import org.scalatest.compatible.Assertion
+import org.scalatest.matchers.should
+import io.github.iltotore.iron.:|
+import io.github.iltotore.iron.constraint.string.{ EndWith, StartWith }
 
-  def test(testName: String): Try[Unit] =
-    for
-      workingDir = prepare(testName)
+trait PlatformTest extends should.Matchers with FileSystem:
+  export PlatformTest.{ ->, Path }
+
+  val snapshotsFolderPath: Path
+
+  val templatePaths: Set[Path]
+
+  val testResourcesPath: Path
+
+  def test(testName: String)(addSubstitutions: SubstitutionBuilder ?=> Unit): Assertion =
+    given builder: SubstitutionBuilder = SubstitutionBuilder()
+    addSubstitutions
+    (for
+      workingDir = testDir(testName)
+      _ <- resolveTemplates(testName, workingDir, builder.all)
       _ <- compile(workingDir)
       out <- run(workingDir)
       _ = scribe.info(s"Test output: $out")
-    yield ()
-
-  def prepare(testName: String): Path =
-    val workingDir = testDir(testName)
-    resolveTemplates(testName, workingDir)
-    workingDir
+    yield out).fold(err => fail(s"Test failed: ${err.getMessage}"), out => compare(out, testName))
 
   def testDir(directoryName: String): Path
 
-  def resolveTemplates(testName: String, workingDir: Path): Unit
+  def resolveTemplates(testName: String, workingDir: Path, substitutions: Set[(Pattern, String)]): Try[Unit] = Try:
+    templatePaths.foreach(template => copy(template, s"$workingDir/${baseName(template)}"))
+    val programContent = read(programContentPath(testName))
+    val templateContent = read(programTemplatePath)
+    val updatedContent = substitutions.foldLeft(templateContent.appendedAll(programContent)):
+      case (content, (pattern, replacement)) =>
+        val newContent = content.replace(pattern, replacement)
+        if newContent == content then throw IllegalStateException(s"No pattern ${pattern} found in content.")
+        else newContent
+    write(s"$workingDir/main.mjs", updatedContent)
+
+  def programContentPath(testName: String): Path
+
+  def programTemplatePath: Path
 
   def compile(workingDir: Path): Try[Unit]
 
   def run(workingDir: Path): Try[String]
+
+  def compare(actual: String, testName: String): Assertion =
+    val expected = Try(read(s"$snapshotsFolderPath/$testName"))
+    if expected.isFailure then fail(s"Missing snapshot for test $testName")
+    else expected.get.trim shouldBe actual.trim
+
 end PlatformTest
+
+trait FileSystem:
+
+  def copy(origin: Path, destination: Path): Unit
+
+  def baseName(path: Path): String
+
+  def read(path: Path): String
+
+  def write(path: Path, content: String): Unit
+
+object PlatformTest:
+  type Path = String
+
+  type Pattern = String :| (StartWith["{{"] & EndWith["}}"])
+
+  class SubstitutionBuilder:
+    private var substitutions = Set.empty[(Pattern, String)]
+    private[PlatformTest] def add(key: Pattern, value: String): Unit = substitutions += ((key, value))
+    def all: Set[(Pattern, String)] = substitutions.view.toSet
+
+  extension (pattern: Pattern)
+    infix inline def ->(value: String)(using builder: SubstitutionBuilder): Unit = builder.add(pattern, value)
