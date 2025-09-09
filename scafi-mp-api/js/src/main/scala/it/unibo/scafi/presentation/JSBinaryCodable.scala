fc4b5d3b6f4742741ce1f14cbca7be0374514117
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-09-09 09:23:40 +0200

    refactor: remove repetition

--- scafi-mp-api/js/src/main/scala/it/unibo/scafi/presentation/JSBinaryCodable.scala
+++ scafi-mp-api/js/src/main/scala/it/unibo/scafi/presentation/JSBinaryCodable.scala
@@ -27,17 +27,17 @@ object JSBinaryCodable:
   given jsBinaryCodable: UniversalCodable[js.Any, Array[Byte]] = new UniversalCodable[js.Any, Array[Byte]]:
     private var registry = JSCodablesRegistry.forStringId(primitiveCodables)
 
-    override def register(value: js.Any): Unit =
-      val codable = value.asPrimitiveCodable.getOrElse(JSCodable(value.asInstanceOf[js.Object]))
-      registry = registry.register(codable)
+    override def register(value: js.Any): Unit = registry = registry.register(codableOf(value))
 
     override def encode(value: js.Any): Array[Byte] =
-      val codable = value.asPrimitiveCodable.getOrElse(JSCodable(value.asInstanceOf[js.Object]))
-      val (format, encodedData) = codable.encode(value.asInstanceOf[js.Object]) match
+      val codable = codableOf(value)
+      val (format, encodedData) = codable.encode(value) match
         case bytes: Uint8Array => (Format.Binary, bytes.toByteArray)
         case s if js.typeOf(s) == "string" => (Format.String, s.asInstanceOf[String].getBytes(StandardCharsets.UTF_8))
         case other => throw new IllegalArgumentException(s"$other (${js.typeOf(other)}) is not a supported format.")
       Cbor.encode(codable.typeName, format, encodedData).toByteArray
+
+    private def codableOf(value: js.Any) = value.asPrimitiveCodable.getOrElse(JSCodable(value.asInstanceOf[js.Object]))
 
     override def decode(bytes: Array[Byte]): js.Any =
       val (typeName, format, encodedData) = Cbor.decode(bytes).to[(String, Format, Array[Byte])].value
