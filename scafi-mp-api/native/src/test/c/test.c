47614768948a42b99b33a9e48245b3e62d084215
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-09-17 21:50:48 +0200

    custom c types

--- scafi-mp-api/native/src/test/c/test.c
+++ scafi-mp-api/native/src/test/c/test.c
@@ -11,11 +11,17 @@ int main(void) {
 
     Map m = map_empty();
     Map m1 = map_put(m, (void*)1, (void*)2);
-    printf("map_get 1: %d\n", (int)(long)map_get(m1, (void*)1));
-
     Map m2 = map_put(m1, (void*)3, (void*)4);
-    printf("map_get 1: %d\n", (int)(long)map_get(m2, (void*)1));
-    printf("map_get 3: %d\n", (int)(long)map_get(m2, (void*)3));
+    Map m3 = map_put(m2, (void*)10, (void*)20);
+    Map m4 = map_put(m3, (void*)1, (void*)3); // overwrite? yes
+
+    printf("map_get 1: %d\n", (int)(long)map_get(m4, (void*)1));
+    printf("map_get 3: %d\n", (int)(long)map_get(m4, (void*)3));
+    printf("map_get 10: %d\n", (int)(long)map_get(m4, (void*)10));
+
+    Map m5 = map_of((Tuple2*[]){t1, t2}, 2);
+    printf("map_of get 1: %d\n", (int)(long)map_get(m5, (void*)1));
+    printf("map_of get 3: %d\n", (int)(long)map_get(m5, (void*)3));
 
     return 0;
 }
--- scafi-mp-api/native/src/test/c/test.h
+++ scafi-mp-api/native/src/test/c/test.h
@@ -18,6 +18,14 @@ Map map_empty();
 
 Map map_put(Map map, void* key, void* value);
 
+Map map_of(Tuple2** entries, size_t size) {
+    Map map = map_empty();
+    for (size_t i = 0; i < size; i++) {
+        map = map_put(map, fst(entries[i]), snd(entries[i]));
+    }
+    return map;
+}
+
 void* map_get(const Map map, void* key);
 
 #endif // TEST_H
