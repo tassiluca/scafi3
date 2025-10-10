0f9cfdfb0a2b6956bb0f6403bf7c6044af8b7a4a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-10-10 17:31:18 +0200

    test: add native branch restriction test

--- scafi3-integration/src/test/resources/js/restricted-exchange/program.mjs
+++ scafi3-integration/src/test/resources/js/restricted-exchange/program.mjs
@@ -1,7 +1,7 @@
 function aggregateProgram(lang) {
     return lang.branch(
-        lang.localId % 2 == 0,
-        () => lang.exchange(lang.Field.of(true), n => returnSending(n)),
-        () => lang.exchange(lang.Field.of(false), n => returnSending(n))
+        lang.localId % 2 === 0,
+        () => lang.exchange(lang.Field.of(1), n => returnSending(n)),
+        () => lang.exchange(lang.Field.of(0), n => returnSending(n))
     );
 }
