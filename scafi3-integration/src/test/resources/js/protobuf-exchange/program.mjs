c02e30f7da9148f8db3287466e376e8c0156bcf2
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-10-12 16:48:52 +0200

    refactor: aggregate common native code in ad hoc utilities

--- scafi3-integration/src/test/resources/js/protobuf-exchange/program.mjs
+++ scafi3-integration/src/test/resources/js/protobuf-exchange/program.mjs
@@ -7,10 +7,10 @@ function aggregateProgram(lang) {
     );
 }
 
-proto.TemperatureSensor.prototype.toString = function () {
-    return `Sensor(id=${this.id}, temp=${this.temperature.toFixed(2)})`;
-};
-
 function senseTemperature() {
     return proto.TemperatureSensor.create({ id: `#${deviceId}`, temperature: deviceId * 10 });
 }
+
+proto.TemperatureSensor.prototype.toString = function () {
+    return `Sensor{id=${this.id}, temp=${this.temperature.toFixed(2)}}`;
+};
